8f58d1d80ab1c36b68db0030c9763c15f06aa2fe
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-07-15 13:13:18 +0200

    demo 3

--- client/src/config/baseURL.js
+++ client/src/config/baseURL.js
@@ -1,3 +1,6 @@
-const baseURL = "http://localhost:5000/api/v1";
+const baseURL =
+  process.env.REACT_APP_ENV === "development"
+    ? "http://localhost:5000/api/v1"
+    : "https://blogs-app-mern.herokuapp.com/api/v1";
 
 export default baseURL;
